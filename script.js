1402aecdcfbf025f881cda9018e95566a4dab044
Daniel Feito Pin <danielfeitopin@protonmail.com>
2024-10-16 18:51:15 +0200

    Add missing semicolons

--- script.js
+++ script.js
@@ -2,13 +2,13 @@
 const currentYear = new Date().getFullYear();
 const currentMonth = new Date().getMonth() + 1; // 1 to 12
 const courseRange = (currentMonth >= 9) ? ` ${currentYear}-${currentYear+1}`
-                                        : ` ${currentYear-1}-${currentYear}`
+                                        : ` ${currentYear-1}-${currentYear}`;
 document.getElementById('course').innerText = courseRange;
 
 // Protect email
-const emailUsername = 'representantes'
-const emailDomain = 'dafic.org'
+const emailUsername = 'representantes';
+const emailDomain = 'dafic.org';
 const openEmail = () => {
-    document.getElementById('email').href = `mailto:${emailUsername}@${emailDomain}`
+    document.getElementById('email').href = `mailto:${emailUsername}@${emailDomain}`;
 }
-document.getElementById('email').setAttribute('onclick', 'openEmail();')
+document.getElementById('email').setAttribute('onclick', 'openEmail();');
